f97ccbbbf7ff16bd1aafbc7a645074976530e87f
mpojeda84 <maikel.pereira@gmail.com>
2024-12-03 14:12:15 -0500

    Update checkout-pay-gateway.js

--- checkout-pay-gateway.js
+++ checkout-pay-gateway.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (C) 2024 Maikel Pereira
+ * Copyright (C) 2024 Solvisse Corporation
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
